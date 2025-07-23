c8a7bb9d3c2d3f2f224ff51f60d0e160eaed35b4
muradbdvc <muradbdvc@gmail.com>
2025-07-24 01:45:11 +0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Resume from './components/Resume'
 import Navmenu from './components/Navmenu/Navmenu'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
       <Navmenu />
+    
+    </BrowserRouter>
       <Resume />
     </>
   )
--- src/components/Navmenu/Navmenu.jsx
+++ src/components/Navmenu/Navmenu.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
+const navitems =[
+    {
+        id:1,
+        title:'Home',
+        path:'/',
+        cName:'nav-item'
+    }, 
+    {
+        id:2,
+        title:'Resume',
+        path:'/resume',
+        cName:'nav-item'
+    }
+];
 const Navmenu = () => {
   return (
     <div>
-        
+        <nav className="navbar rounded-md">
+            <ul className="navbar " >
+                {navitems.map((item)=>{
+                    const {id, path, title}= item;
+                    return (
+                        <li key={id} className='cName'>
+                            <Link to={path}>
+                                {title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
     </div>
   )
 }
